4638078133a91729836febe10dbd9edc1adeba60
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-02 20:03:37 +0530

    Passing data to componenet

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
-import Counter from './components/counter'
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
