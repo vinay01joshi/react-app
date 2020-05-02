52f5b916f665ee9ade5cee36a52cc9e34f8efc40
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-02 20:14:10 +0530

    Use componenet children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,6 +13,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
