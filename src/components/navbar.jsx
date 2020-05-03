455d06a53132d589bb8f4700455e65fe5fcd0156
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-03 20:49:18 +0530

    Stateless functional component ========== Only component which we do not need to do with states

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Sateless functional components
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
