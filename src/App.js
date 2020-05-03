428ffebe7e54f439030026533d71f3b391fd6ca6
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-03 20:22:08 +0530

    Adding Navbar Componeent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Movies from './components/movies';
+import Navbar from './components/navbar'
 import Counters from './components/counters';
 
 function App() {
   return (
-    <main className="container">
-      <Counters />
-    </main>
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
+
   );
 }
 
