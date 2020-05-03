e1316b6954c6d4ccfe3ee2d7fa2eef9e43b0b17c
Vinay Joshi <vinay01joshi@gmail.com>
2020-05-03 20:44:31 +0530

    Lifing State up for sync parallel componenet can share data.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/movies';
 import Navbar from './components/navbar'
 import Counters from './components/counters';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
-  );
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = this.state.counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete} />
+        </main>
+      </React.Fragment>
+
+    );
+  };
+
 }
 
 export default App;
